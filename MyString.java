efe1956b207a819807ed97c531a77ca52807f936
Jackson Zou <jzou00@stuy.edu>
2018-11-21 10:50:34 -0500

    Created charAt(), length(), subSequence(), and toString() methods

--- MyString.java
+++ MyString.java
@@ -7,4 +7,31 @@ public class MyString implements CharSequence{
     }
   }
 
+  public char charAt(int index) {
+    try {
+      return data[i];
+    }catch(ArrayIndexOutOfBounds e) {
+      System.out.println("Invalid index");
+    }
+  }
+
+  public int length() {
+    return data.length;
+  }
+
+  public CharSequence subSequence(int start, int end) {
+    CharSequence f = new CharSequence();
+    for (int i = start; i < end; i++) {
+      f+= data[i];
+    }
+    return f;
+  }
+
+  public String toString() {
+    String f = "";
+    for (int i = 0; i < data.length; i++) {
+      f+= data[i];
+    }
+    return f;
+  }
 }
